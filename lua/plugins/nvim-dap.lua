66ac15b26b8c109adcd50be30225c94e2c5d0e51
ClearAspect <roanmason@live.ca>
2025-05-16 01:44:41 -0400

    dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,14 @@
+return {
+	-- "mfussenegger/nvim-dap",
+	-- -- event = { "VeryLazy" },
+	-- -- dependencies = { "" },
+	-- -- lazy = false,
+	-- -- ft = { "" },
+	-- config = function()
+	-- 	local dap = require("dap")
+	--
+	-- 	dap.adapters.c
+	--
+	--
+	-- end,
+}
